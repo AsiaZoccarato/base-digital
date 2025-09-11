ddd8e336846af8bd9340a221fbbecbb3ee81a87f
AsiaZoccarato <asia.zoccarato@edu.unito.it>
2025-09-11 19:22:44 +0200

    Initial commit - Vue base project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App).use(store).use(router).mount("#app");
+// Importa CSS di Bootstrap e icone
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+// Importa JS di Bootstrap (per dropdown, modali, ecc.)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+createApp(App).use(store).use(router).mount('#app')
